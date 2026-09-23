35dee05a1ab1d581e1ce84d8e4be09b7d8eb18c2
Caleb Gruber <79333667+calebgruber@users.noreply.github.com>
2026-09-23 00:33:31 -0400

    Add local configuration for database connection

---
+++ config.local.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', 3306);
+define('DB_NAME', 'voxelnodes_pe');
+define('DB_USER', 'voxelnodes_pe');
+define('DB_PASS', 'OO1--#]p$?jC+Lf4');
+define('APP_BASE_URL', '/your-cpanel-folder');
