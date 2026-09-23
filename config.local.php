3c80e3200db2cb03dd48e4c63411977b1ca79921
Caleb Gruber <79333667+calebgruber@users.noreply.github.com>
2026-09-22 20:44:52 -0400

    Update database configuration in config.local.php

---
+++ config.local.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', 3306);
+define('DB_NAME', 'voxelnodes_pe');
+define('DB_USER', 'voxelnodes_pe');
+define('DB_PASS', 'Ngw}!=r{8R?}%&R8');
+define('APP_BASE_URL', '/');
--- config_local.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_DRIVER', 'mysql');
-define('DB_HOST', '127.0.0.1');
-define('DB_PORT', 3306);
-define('DB_NAME', 'pe_work');
-define('DB_USER', 'your_user');
-define('DB_PASS', 'your_password');
-define('APP_BASE_URL', '/your-cpanel-folder');
