b95caf887c22bba23fcde02aeb80ec178e1a81e1
Caleb Gruber <79333667+calebgruber@users.noreply.github.com>
2026-09-23 13:43:11 -0400

    Add local configuration for database connection

---
+++ config.local.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', 3306);
+define('DB_NAME', 'voxelnodes_pe');
+define('DB_USER', 'voxelnodes_pe');
+define('DB_PASS', 'sH{W3lv@!1&$s.^?');
+define('APP_BASE_URL', '/');
